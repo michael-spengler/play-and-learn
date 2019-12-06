469dc67b464bd7717396c722cdf8b4fca4fc3c86
michael-spengler <>
2019-12-06 16:53:53 +0100

    define question and answer pairs feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export interface IQAPair {
+  question: string;
+  answer: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'play-and-learn';
+  public title = 'play-and-learn';
+
+  public qaPair: IQAPair = {
+    question: '',
+    answer: ''
+  };
+
+  public qaPairCollection: IQAPair[] = [];
+
+
+  public clickSave() {
+    this.qaPairCollection.push(this.qaPair);
+    this.qaPair = {
+      question: '',
+      answer: ''
+    };
+  }
+
 }
